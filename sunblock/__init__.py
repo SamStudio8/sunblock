8103d1a6a7c91cd09489583adfeaa0f286c885c3
Sam Nicholls <sam@samnicholls.net>
2015-03-25 16:50:15 +0000

    Save and load config as JSON. deal with it.

--- sunblock/__init__.py
+++ sunblock/__init__.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 import click
@@ -26,11 +27,36 @@ def init(template):
     for key, conf in sorted(job.config.items(), key=lambda x: x[1]["order"]):
         job.set_key(key, click.prompt(conf["prompt"], type=conf["type"]))
 
+    # Write
+    to_write = {"template": template}
+    for key, conf in job.config.items():
+        to_write[key] = conf["value"]
+
+    out_fh = open("job.out", "w")
+    out_fh.write(json.dumps(to_write))
+    out_fh.close()
+
+@cli.command(help="Execute a configured job")
+@click.argument('config', type=click.Path(exists=True, readable=True))
+def execute(config):
+
+    # Open
+    in_fh = open(config)
+    config = json.loads(in_fh.read())
+    in_fh.close()
+
+    # Load
+    job = util.get_template_list()[config["template"]]()
+    for key, conf in sorted(job.config.items(), key=lambda x: x[1]["order"]):
+        job.set_key(key, config[key])
+
     # Check config
     #TODO More helpful output -- what key is missing or incorrect?
     if not job.check():
         log.error("Invalid job configuration. Aborting.")
         sys.exit(1)
+
+    job.execute()
 
 
 @cli.command(help="List available job configuration templates")
