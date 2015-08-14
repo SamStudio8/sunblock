744c0a1efc5bf047d1fe08deb9de3d104b0bb929
Sam Nicholls <sam@samnicholls.net>
2015-08-14 12:30:03 +0100

    threads and memsize for sort

--- sunblock/jobs/samtools_sort.py
+++ sunblock/jobs/samtools_sort.py
@@ -14,12 +14,12 @@ class SAMToolsSort(job.Job):
 
         self.add_key("file", "Path to file to sort", "File to sort", Path(exists=True, readable=True, writable=True, resolve_path=True))
         self.add_key("out", "Path to file to sorted output", "Output file", Path(writable=True, resolve_path=True))
-        self.add_key("threads", "numberof sorting threads", "num sort threads")
-        #self.add_key("readname", "Sort by readname", "Readname flag", BOOL)
+        self.add_key("threads", "number of sorting threads", "num sort threads", int)
+        self.add_key("mem_size", "max mem size", "max mem size", str)
 
     def define(self, shard=None):
         self.set_commands([
-            ("/ibers/ernie/home/msn/git/samtools/samtools sort -@ %d -T %s -Obam -o %s %s" % (self.config["threads"]["value"], self.config["file"]["value"], self.config["out"]["value"], self.config["file"]["value"]))
+            ("/ibers/ernie/home/msn/git/samtools/samtools sort -m %s -@ %d -T %s -Obam -o %s %s" % (self.config["mem_size"]["value"], self.config["threads"]["value"], self.config["file"]["value"], self.config["out"]["value"], self.config["file"]["value"]))
         ])
         self.add_post_checksum(self.config["out"]["value"])
 
